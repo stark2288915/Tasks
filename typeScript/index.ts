c6e26ade015139f5e2c26230a44bfd82d983e97a
stark2288915 <stark2288915@gamil.com>
2023-12-26 11:01:21 +0200

    7щ768щ

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,1 +1,44 @@
 console.log("Hello");
+
+const func = (name:string) => {
+    console.log(name.length);
+}
+func("Sergey");
+
+interface ICar {
+    name: string;
+    model: string;
+    engy?: number;
+}
+
+const car:ICar = {
+    name: "Mitsybishi",
+    model: "lancer",
+    engy: 1.6
+}
+
+interface IBake<DATA> {
+    name: string;
+    data: DATA
+}
+
+const mustang:IBake<number> ={
+    name: "mustang",
+    data: 4,
+}
+const alpha:Partial<IBake<number>> ={
+    data: 8
+}
+
+class Cars {
+    private power: number;
+    private name: string;
+    constructor(name: string, power: number) {
+        this.name = name;
+        this.power = power;
+    }
+
+    getName(): string{
+        return this.name;
+    }
+}
