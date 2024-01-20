2fac30d3eddb7f2d876a65a66a14c9e6b365eb09
stark2288915 <stark2288915@gamil.com>
2024-01-20 21:44:40 +0200

    7щ768щ

--- sqlDB/lesson_2.sql
+++ sqlDB/lesson_2.sql
@@ -1,35 +1,27 @@
-use sergey;
+-- 1.Вибрати усіх клієнтів, чиє ім'я має менше ніж 6 символів.
+--
+-- 2.Вибрати львівські відділення банку.
+--
+-- # 3.Вибрати клієнтів з вищою освітою та посортувати по прізвищу.
+-- #
+-- # 4.Виконати сортування у зворотньому порядку над таблицею Заявка і вивести 5 останніх елементів.
+-- #
+-- # 5.Вивести усіх клієнтів, чиє прізвище закінчується на OV чи OVA.
 
-create table cities(
-                       id int primary key auto_increment,
-                       city varchar(25) not null
-)
-drop table users;
-drop table cities;
+select * from client where length(FirstName) > 6;
 
-/*create table users (
-    id int primary key auto_increment,
-    name varchar(20) not null,
-    age int not null,
-    city_id int null,
-    foreign key (city_id) references cities(id)
-)*/
+select * from department where DepartmentCity = 'Lviv';
 
+select * from client where Education = 'high' order by LastName;
 
-select * from users join cities on cities.id = users.city_id;
-select users.id, name, age, city from users join cities on cities.id = users.city_id;
+select * from application order by Currency desc limit 5;
 
-select * from cities c join users u on c.id = u.city_id;
-select * from cities c left join users u on c.id = u.city_id;
-select * from cities c right join users u on c.id = u.city_id;
-
-select users.* from users join cities c on c.id = users.city_id where  city = "Lviv";
-
-select distinct name from users;
-select sum(users.age) from users
-union
-select max(users.age) from users;
-
-select age from users where gender = 'female';
-select * from cars where price in (select age from users where gender = 'male');
-
+-- # 6.Вивести клієнтів банку, які обслуговуються київськими відділеннями.
+-- #
+-- # 7.Знайти унікальні імена клієнтів.
+-- #
+-- # 8.Вивести дані про клієнтів, які мають кредит більше ніж на 5000 гривень.
+-- #
+-- # 9.Порахувати кількість клієнтів усіх відділень та лише львівських відділень.
+-- #
+-- # 10.Знайти кредити, які мають найбільшу суму для кожного клієнта окремо.
