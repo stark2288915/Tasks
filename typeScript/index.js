df6228a7f40f1ca7b2be08908cf27cec2f25053d
stark2288915 <stark2288915@gamil.com>
2024-01-22 22:33:31 +0200

    7щ768щ

--- typeScript/index.js
+++ typeScript/index.js
@@ -3,3 +3,25 @@ var func = function (name) {
     console.log(name.length);
 };
 func("Sergey");
+var car = {
+    name: "Mitsybishi",
+    model: "lancer",
+    engy: 1.6
+};
+var mustang = {
+    name: "mustang",
+    data: 4,
+};
+var alpha = {
+    data: 8
+};
+var Cars = /** @class */ (function () {
+    function Cars(name, power) {
+        this.name = name;
+        this.power = power;
+    }
+    Cars.prototype.getName = function () {
+        return this.name;
+    };
+    return Cars;
+}());
---
+++ typeScript/index_2.ts
@@ -0,0 +1,4 @@
+let a: string = 'hello';
+let b: number = 28;
+let c: string = 'sergey';
+console.log(`${a} ${b} ${c}`);
